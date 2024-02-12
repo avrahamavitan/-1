c6fa5e5f16abf9982a73b4b58fc9eb7fd2154e57
avrahamavitan <155629131+avrahamavitan@users.noreply.github.com>
2024-02-12 21:08:26 +0200

    Add files via upload

---
+++ my_mat.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+int knapSack(int weights[], int values[], int selected_bool[]);
+int max(int a, int b);
+#define w (20)
+#define size (5)
+void fanc1(int arr[][10]);
+void fanc2(int arr[][10]);
+void fanc3(int arr[][10]);
+void algoritem(int arr[10][10]);
